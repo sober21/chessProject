140753ec6da69e9a60dca5bd5d2d123314e391c7
trezvy_papa <mix_mmg@mail.ru>
2023-10-10 23:22:20 +0300

    Возможные ходы собираются в список. Фильтруется ввод хода в цикле. Нужно сделать, чтобы ввод хода фильтровался по списку возможных ходов и чтобы в функцию, которая возвращает этот список попадали только правильные ходы.

--- main/figures/rock.py
+++ main/figures/rock.py
@@ -1,6 +1,41 @@
-def rock_move(position:str) ->None:
-    a, b = position
+def possible_move(start_position='a1') -> list:
     row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    possible_move_rock_list = []
+    a, b = start_position
+    for i in range(8):
+        for j in range(8):
+            if (j == row.index(a) and (8 - int(b) > i or 8 - int(b) < i)) or (
+                    8 - int(b) == i and (j > row.index(a) or j < row.index(a))):
+                position = row[j] + str(8 - i)
+                possible_move_rock_list.append(position)
+    return possible_move_rock_list
+
+
+def valid_move(move='00') -> str:
+    row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    while move[0] not in row or move[1] not in ['1', '2', '3', '4', '5', '6', '7', '8']:
+        move = input('Сделайте ход:').lower().strip()
+        if move == 'стоп':
+            return 'Пока!'
+        elif move not in possible_move():
+            print('Невозможный ход для ладьи.')
+        elif len(move) == 0:
+            print('Надо написать ход в шахматной аннотации')
+            move = 'q0'
+        elif len(move) != 2:
+            print('Должно быть 2 символа без пробела: ряд и вертикаль.')
+
+        elif not move.isalnum():
+            print('Вводите только букву и цифру, как на шахматной доске.')
+
+        else:
+            print('Такой клетки нет на шахматной доске.')
+    return move
+
+
+def rock_move() -> None:
+    row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    a, b = valid_move()
     for i in range(8):
         for j in range(8):
             if j == row.index(a) and i == 8 - int(b):
@@ -9,10 +44,10 @@ def rock_move(position:str) ->None:
                     8 - int(b) == i and (j > row.index(a) or j < row.index(a))):
                 print('*', end=' ')
             else:
-                print('.',end=' ')
+                print('.', end=' ')
         print()
+    print(end='\n \n \n')
 
 
 if __name__ == '__main__':
-    move = input('Сделайте ход:')
-    rock_move(move)
+    rock_move()
