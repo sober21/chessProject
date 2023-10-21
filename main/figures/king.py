561cff2a87149bb7487b1291854594d72d897017
Trezvy_Papa <mix_mmg@mail.ru>
2023-10-21 11:18:32 +0300

    Оптимизировал фукции фигур. Теперь вся реализация ходов находится в одной функции possible_move_figure

--- main/figures/king.py
+++ main/figures/king.py
@@ -1,24 +1,24 @@
 def possible_move_king(a_position='e1') -> list:
     '''возвращает список возможных ходов для конкретной позиции'''
 
-    row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    start_row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     possible_move_king_list = []
     a, b = a_position
     for i in range(8):
         for j in range(8):
-            if (j == row.index(a) and (8 - int(b) == i + 1 or 8 - int(b) == i - 1)) or (
-                    8 - int(b) == i and (j - 1 == row.index(a) or j + 1 == row.index(a))) or (
-                         8 - int(b) == i - 1 and row.index(a) == j - 1) or (
-                         8 - int(b) == i + 1 and row.index(a) == j + 1) or (
-                         8 - int(b) == i - 1 and row.index(a) == j + 1) or (
-                         8 - int(b) == i + 1 and row.index(a) == j - 1):
-                position = row[j] + str(8 - i)
+            if (j == start_row.index(a) and (8 - int(b) == i + 1 or 8 - int(b) == i - 1)) or (
+                    8 - int(b) == i and (j - 1 == start_row.index(a) or j + 1 == start_row.index(a))) or (
+                         8 - int(b) == i - 1 and start_row.index(a) == j - 1) or (
+                         8 - int(b) == i + 1 and start_row.index(a) == j + 1) or (
+                         8 - int(b) == i - 1 and start_row.index(a) == j + 1) or (
+                         8 - int(b) == i + 1 and start_row.index(a) == j - 1):
+                position = start_row[j] + str(8 - i)
                 possible_move_king_list.append(position)
     return possible_move_king_list
 
 
 def king_move() -> None:
-    row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    start_row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     list_move = ['e1']
     while True:
         cur_move = input('Сделайте ход:')
@@ -28,14 +28,9 @@ def king_move() -> None:
         a, b = cur_move
         for i in range(8):
             for j in range(8):
-                if j == row.index(a) and i == 8 - int(b):
+                if j == start_row.index(a) and i == 8 - int(b):
                     print('K', end=' ')
-                elif (j == row.index(a) and (8 - int(b) == i+1 or 8 - int(b) == i-1)) or (
-                        8 - int(b) == i and (j-1 == row.index(a) or j+1 == row.index(a))) or (
-                        8 - int(b) == i - 1 and row.index(a) == j - 1) or (
-                        8 - int(b) == i + 1 and row.index(a) == j + 1) or (
-                        8 - int(b) == i - 1 and row.index(a) == j + 1) or (
-                        8 - int(b) == i + 1 and row.index(a) == j - 1):
+                elif start_row[j] + str(8-i) in possible_move_king(cur_move):
                     print('*', end=' ')
                 else:
                     print('.', end=' ')
--- main/figures/knight.py
+++ main/figures/knight.py
@@ -2,21 +2,21 @@
 def possible_move_knight(a_position='g1') -> list:
     '''возвращает список возможных ходов для конкретной позиции'''
 
-    row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    start_row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     possible_move_knight_list = []
     a, b = a_position
     for i in range(8):
         for j in range(8):
-            if (8 - int(b) - 2 == i and j == row.index(a) - 1) or (8 - int(b) - 2 == i and j == row.index(a) + 1) \
-                 or (8 - int(b) + 2 == i and j == row.index(a) - 1) or (8 - int(b) + 2 == i and j == row.index(a) + 1) \
-                 or (8 - int(b) - 1 == i and j == row.index(a) - 2) or (8 - int(b) - 1 == i and j == row.index(a) + 2) \
-                 or (8 - int(b) + 1 == i and j == row.index(a) - 2) or (8 - int(b) + 1 == i and j == row.index(a) + 2):
-                position = row[j] + str(8 - i)
+            if (8 - int(b) - 2 == i and j == start_row.index(a) - 1) or (8 - int(b) - 2 == i and j == start_row.index(a) + 1) \
+                 or (8 - int(b) + 2 == i and j == start_row.index(a) - 1) or (8 - int(b) + 2 == i and j == start_row.index(a) + 1) \
+                 or (8 - int(b) - 1 == i and j == start_row.index(a) - 2) or (8 - int(b) - 1 == i and j == start_row.index(a) + 2) \
+                 or (8 - int(b) + 1 == i and j == start_row.index(a) - 2) or (8 - int(b) + 1 == i and j == start_row.index(a) + 2):
+                position = start_row[j] + str(8 - i)
                 possible_move_knight_list.append(position)
     return possible_move_knight_list
 
 def knight_move() -> None:
-    row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    start_row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
     list_move = ['g1']
     while True:
         cur_move = input('Сделайте ход:')
@@ -26,12 +26,9 @@ def knight_move() -> None:
         a, b = cur_move
         for i in range(8):
             for j in range(8):
-                if j == row.index(a) and i == 8 - int(b):
+                if j == start_row.index(a) and i == 8 - int(b):
                     print('N', end=' ')
-                elif (8 - int(b) - 2 == i and j == row.index(a) - 1) or(8 - int(b) - 2 == i and j == row.index(a) + 1)\
-                  or (8 - int(b) + 2 == i and j == row.index(a) - 1) or (8 - int(b) + 2 == i and j == row.index(a) + 1)\
-                  or (8 - int(b) - 1 == i and j == row.index(a) - 2) or (8 - int(b) - 1 == i and j == row.index(a) + 2)\
-                  or (8 - int(b) + 1 == i and j == row.index(a) - 2) or (8 - int(b) + 1 == i and j == row.index(a) +2):
+                elif start_row[j] + str(8 - i) in possible_move_knight(cur_move):
                     print('*', end=' ')
                 else:
                     print('.', end=' ')
